b60dfd7486b5d12b291e20a301bd19e3ce9e02a2
Alisa Vorotyntseva <alisa.vorotyn@gmail.com>
2022-11-28 16:13:07 -0800

    Build App.js

--- src/App.js
+++ src/App.js
@@ -1,8 +1,20 @@
 import './App.css';
+import AngryButton from './components/AngryButton';
+import CounterButton from './components/CounterButton';
+import LightSwitchButton from './components/LightSwitchButton';
+import TextRepeaterButton from './components/TextRepeaterButton';
+
 
 function App() {
   return (
     <div className="App">
+      <h1>Fancy Buttons!</h1>
+      <section>
+        <AngryButton />
+        <CounterButton />
+        <LightSwitchButton />
+        <TextRepeaterButton />
+      </section>
     </div>
   );
 }
